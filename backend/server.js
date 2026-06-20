af142a89a0906ccf358890545f6011d5035fb369
Potti Mokshagna <pottimokshagna@gmail.com>
2026-06-21 01:01:26 +0530

    Updated packages

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const tripRoutes = require('./routes/trips');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
